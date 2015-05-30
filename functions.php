e2659539ca06fb8a3f62c8f61a5f09e6ce215c7d
Max Norton <maxbnorton@gmail.com>
2015-05-29 17:27:33 -0700

    debug google font enqueue function

--- functions.php
+++ functions.php
@@ -2,14 +2,6 @@
 	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 	function theme_enqueue_styles() {
 	    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
+		wp_enqueue_style( 'custom_fonts', '//fonts.googleapis.com/css?family=Cagliostro' );
 	}
-	function google_fonts() {
-		$query_args = array(
-		'family' => 'Cagliostro'
-		'subset' => 'latin,latin-ext',
-		);
-		wp_register_style( 'google_fonts', add_query_arg( $query_args, "//fonts.googleapis.com/css" ), array(), null );
-	}
-	add_action('wp_enqueue_scripts', 'google_fonts'); 
 ?>
