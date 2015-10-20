9ae8fff54a3e5c6dd408401acf7e2dac22809165
Max Norton <maxbnorton@gmail.com>
2015-10-19 18:41:03 -0700

    style grid-shop section

--- loops/loop-shop.php
+++ loops/loop-shop.php
@@ -7,13 +7,15 @@
 <?php /* Start the Loop */ ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<?php
-		/* Include the Post-Format-specific template for the content.
-		* If you want to override this in a child theme, then include a file
-		* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part( 'content-book', get_post_format() );
-	?>
+	<section class="grid-shop">
+		<?php
+			/* Include the Post-Format-specific template for the content.
+			* If you want to override this in a child theme, then include a file
+			* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
+			get_template_part( 'content-book', get_post_format() );
+		?>
+	</section>
 
 	<?php endwhile; ?>
 
