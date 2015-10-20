ddc69cd7e78210db0dc4cfec0dacbf72c1fc9d9d
Max Norton <maxbnorton@gmail.com>
2015-10-19 18:49:59 -0700

    hide shop item header, set padding

--- content-book.php
+++ content-book.php
@@ -16,14 +16,14 @@
 			<?php the_post_thumbnail(); ?>
 		</div>				
 	<?php endif; ?>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+	<!--header class="entry-header">
+		<!?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
+		<!?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php ultra_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+			<!?php ultra_posted_on(); ?>
+		</div><!-- .entry-meta >
+		<!?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
