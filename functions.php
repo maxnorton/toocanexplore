c0c8c8129ca3e79cf9708dfd2d8994de6402fabc
Max Norton <maxbnorton@gmail.com>
2016-02-12 13:30:21 -0800

    generate tce_book post type

--- functions.php
+++ functions.php
@@ -4,4 +4,17 @@
 	    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 		wp_enqueue_style( 'custom_fonts', '//fonts.googleapis.com/css?family=Cagliostro' );
 	}
+	add_action( 'init', 'create_post_type' );
+	function create_post_type() {
+	  register_post_type( 'tce_book',
+	    array(
+	      'labels' => array(
+	        'name' => __( 'Books' ),
+	        'singular_name' => __( 'Book' )
+	      ),
+	      'public' => true,
+	      'has_archive' => false,
+	    )
+	  );
+	}
 ?>
