bb9772764dc6f89eaaa9543656d18c433d16889a
Max Norton <maxbnorton@gmail.com>
2016-02-18 16:15:54 -0800

    hide sidebar and post navigation on book template colorboxes

--- single-tce_book.php
+++ single-tce_book.php
@@ -13,8 +13,6 @@
 
 			<?php get_template_part( 'content', 'single' ); ?>
 
- 			<?php if ( siteorigin_setting( 'navigation_post_nav' ) ) the_post_navigation(); ?>
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || get_comments_number() ) :
